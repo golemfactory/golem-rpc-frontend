71b835615e45f5ac8103f0baa75af2e5b60225ea
scx1332 <sieciech@gmail.com>
2022-08-23 12:43:40 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import ProgressPlot from "./ProgressPlot";
-
+import configData from "./config.json";
 
 function App() {
 
+
 	return (
 		<div className="App">
+			<div>
+				Config data:
+				{JSON.stringify(configData, null, 2)}
+			</div>
 			<ProgressPlot></ProgressPlot>
 		</div>
 	);
