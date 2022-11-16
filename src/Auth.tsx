1b107b049bdfeb77f0c7e7c0f806a5efe0babf27
scx1332 <sieciech.czajka@golem.network>
2022-11-16 17:07:17 +0100

    Added token checking

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,5 +1,6 @@
 import React, {createContext, useContext, useState} from "react";
 import gatewayProvider from "./GatewayProvider";
+import config from "./config.json";
 
 
 export const AuthContext = createContext(null)
@@ -7,11 +8,19 @@ export const useAuth = () => useContext(AuthContext)
 
 // @ts-ignore
 export const AuthProvider = ({children}) => {
-    const [token, setTokenState] = useState(null);
+    const getInitialState = function() : string {
+        console.log("getInitialState");
+        let token = localStorage.getItem("token") || "";
+        gatewayProvider.setToken(token);
+        return token;
+    }
+
+    const [token, setTokenState] = useState(getInitialState());
 
     const setToken = function (val: string) {
         gatewayProvider.setToken(val);
         setTokenState(val);
+        localStorage.setItem('token', val);
     }
 
     return (
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,16 +1,35 @@
 import './MainPage.css';
 import configData from "./config.json";
 import { Routes, Route, Link } from "react-router-dom";
-import React, {useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Dashboard from "./Dashboard";
 import ClientDashboard from "./ClientDashboard";
 import Yagna from "./Yagna";
 import {useAuth} from "./Auth";
 import gatewayProvider from "./GatewayProvider";
+import config from "./config.json";
 
 function MainPage() {
 	const {token, setToken} = useAuth();
+	const [tokenMsg, setTokenMsg] = useState("");
 	const inputTokenRef = useRef(null);
+
+	const checkToken = function (token: string) {
+		const response = fetch(`${config.BACKEND_URL}hello/${token}`).then(response => {
+			if (response.status === 200) {
+				console.log("Token is valid");
+			}
+			response.json().then(data => {
+				console.log(data);
+				setTokenMsg(data.token);
+				setToken(token);
+			});
+		})
+	}
+
+	useEffect(() => {
+		checkToken(token);
+	}, []);
 
 	return (
 		<div className="App">
@@ -30,10 +49,13 @@ function MainPage() {
 				<Routes>
 					<Route path="/" element={<div>
 						<div>
-							<div>Admin Token: {token}</div>
+							<div>Admin Token: {token} - {tokenMsg}</div>
 							<div>
 								<input ref={inputTokenRef} defaultValue={token}></input>
-								<button onClick={() => setToken(inputTokenRef.current.value)}>Set token</button>
+								<button onClick={() => checkToken(inputTokenRef.current.value)}>Set token</button>
+							</div>
+							<div>
+								<a href={configData.BACKEND_URL + "info/" + token}>Backend api: {configData.BACKEND_URL + "info/" + token}</a>
 							</div>
 							Config data:
 							{JSON.stringify(configData, null, 2)}
